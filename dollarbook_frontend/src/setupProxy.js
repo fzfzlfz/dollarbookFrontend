e48e82d3858311e66e1120b6ad3396381fbbc8d8
FuzhenLi <lifuzhen006@gmail.com>
2022-11-29 20:23:07 -0500

    update

--- dollarbook_frontend/src/setupProxy.js
+++ dollarbook_frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3000',
+      target: 'http://linserv1.cims.nyu.edu:21019/',
       changeOrigin: true,
     })
   );
