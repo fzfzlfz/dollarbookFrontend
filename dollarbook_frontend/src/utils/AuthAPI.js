7c2aa57d6216d7884a4edfff53637b668ffda91d
FuzhenLi <lifuzhen006@gmail.com>
2022-11-29 20:21:28 -0500

    lucky

--- dollarbook_frontend/src/utils/AuthAPI.js
+++ dollarbook_frontend/src/utils/AuthAPI.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import {authHeader} from './AuthUtils';
 
 
-export const login = (email, password) => axios.post(`/login?email=${email}&password=${password}`, {}, {headers: authHeader()});
+export const login = (email, password) => axios.post(`/api/login?email=${email}&password=${password}`, {}, {headers: authHeader()});
 
-export const register = (email, password) => axios.post(`/register?email=${email}&password=${password}`, {}, {headers: authHeader()});
+export const register = (email, password) => axios.post(`/api/register?email=${email}&password=${password}`, {}, {headers: authHeader()});
