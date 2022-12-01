9948af9a4adcc27b227d7dba13fea2371550e4dc
FuzhenLi <lifuzhen006@gmail.com>
2022-12-01 09:07:41 -0500

    changes

--- dollarbook_frontend/src/setupProxy.js
+++ dollarbook_frontend/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3000',
-      // target: 'http://linserv1.cims.nyu.edu:21019/',
+      // target: 'http://localhost:3000',
+      target: 'http://linserv1.cims.nyu.edu:21019/',
       changeOrigin: true,
     })
   );
