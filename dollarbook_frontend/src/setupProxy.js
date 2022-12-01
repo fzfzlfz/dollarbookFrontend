a0d3d49eb5e5ff0dfd613d3e0590006dcea9f950
FuzhenLi <lifuzhen006@gmail.com>
2022-11-30 21:42:51 -0500

    try

---
+++ dollarbook_frontend/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      // target: 'http://linserv1.cims.nyu.edu:21019/',
+      changeOrigin: true,
+    })
+  );
+};
--- dollarbook_frontend/src/utils/TableAPI.js
+++ dollarbook_frontend/src/utils/TableAPI.js
@@ -3,10 +3,10 @@ import {authHeader} from './AuthUtils';
 
 
 // export const getAll = () => axios.get(`${api}/api/v1/tableline`);
-export const getAll = (userid) => axios.get(`/getAll?userid=${userid}`, {headers: authHeader()});
+export const getAll = (userid) => axios.get(`/api/getAll?userid=${userid}`, {headers: authHeader()});
 
-export const create = (body) => axios.post(`/addTransaction`,body, {headers: authHeader()});
+export const create = (body) => axios.post(`/api/addTransaction`,body, {headers: authHeader()});
 
-export const update = (id, date, amount, category, comment) => axios.get(`/updateTransaction?id=${id}&date=${date}&amount=${amount}&category=${category}&comment=${comment}`);
+export const update = (id, date, amount, category, comment) => axios.get(`/api/updateTransaction?id=${id}&date=${date}&amount=${amount}&category=${category}&comment=${comment}`);
 
-export const remove = (id) => axios.get(`/deleteTransaction?id=${id}`);
+export const remove = (id) => axios.get(`/api/deleteTransaction?id=${id}`);
